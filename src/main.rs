be3138d50a02c5a7d64ae1605b00176a4ebed97c
Abhishek Patel <5524161+Abhishek8394@users.noreply.github.com>
2020-05-25 19:46:47 -0700

    tut complete

--- src/main.rs
+++ src/main.rs
@@ -78,6 +78,40 @@ fn generate_workout(intensity: u32, random_number: u32){
     }
 }
 
+#[derive(PartialEq, Debug)]
+struct Shoe{
+    size: u32,
+    style: String,
+}
+
+struct Counter{
+    count: u32,
+}
+
+impl Counter{
+    fn new() -> Counter{
+        Counter{count: 0}
+    }
+}
+
+impl Iterator for Counter{
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item>{
+        if self.count < 5{
+            self.count += 1;
+            return Some(self.count);
+        }else{
+            return None;
+        }
+
+    }
+}
+
+fn shoes_in_my_size(shoes: Vec<Shoe>, size: u32) -> Vec<Shoe>{
+    shoes.into_iter().filter(|shoe| {shoe.size == size}).collect()
+}
+
 fn main() {
     let x = |c| {c};
     let foo = x(String::from("foo"));
@@ -89,6 +123,13 @@ fn main() {
 
     generate_workout(user_specified_value, random_num);
     println!("Done!");
+
+    println!("Iterators..");
+    let v1: Vec<i32> = vec![1, 2, 3];
+    let vp1 = v1.iter().map(|x| x + 1);
+    for i in vp1 {
+        println!("iter: {:?}", i);
+    }
 
 }
 
@@ -110,5 +151,48 @@ mod tests{
         let v1 = cache.value("four");
         let v2 = cache.value("five");
         assert_eq!(v2, "five");
+    }
+
+    #[test]
+    fn filters_by_size(){
+        let shoes = vec![
+            Shoe{size: 1, style: String::from("white")},
+            Shoe{size: 1, style: String::from("black")},
+            Shoe{size: 2, style: String::from("orange")},
+            Shoe{size: 3, style: String::from("blue")},
+        ];
+
+        let exp  = vec![
+            Shoe{size: 1, style: String::from("white")},
+            Shoe{size: 1, style: String::from("black")},
+        ];
+
+        let recv = shoes_in_my_size(shoes, 1);
+        assert_eq!(exp, recv);
+        // below will error because we gave ownership to shoes_in_my_size.
+        // for shoe in shoes.iter(){
+        //     println!("{:?}", *shoe);
+        // }
+    }
+
+    #[test]
+    fn calling_next_directly(){
+        let mut counter = Counter::new();
+        assert_eq!(Some(1), counter.next());
+        assert_eq!(Some(2), counter.next());
+        assert_eq!(Some(3), counter.next());
+        assert_eq!(Some(4), counter.next());
+        assert_eq!(Some(5), counter.next());
+        assert_eq!(None, counter.next());
+    }
+
+    #[test]
+    fn other_iter_methods_on_counter(){
+        let mut counter = Counter::new();
+        let s:u32 = counter.zip(Counter::new().skip(1))
+            .map(|(x, y)| {x + y})
+            .filter(|x| {x % 3 == 0})
+            .sum();
+        assert_eq!(12, s);
     }
 }
